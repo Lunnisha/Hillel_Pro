72954d338dfd7ceae07280709eb7ec09b247204f
Lunnisha <107049777+Lunnisha@users.noreply.github.com>
2022-09-07 15:13:26 +0300

    done HW2

---
+++ scripts/scripts-1.js
@@ -0,0 +1,53 @@
+let myName = "Anna";
+
+let mySurmame = "Serhiivna";
+
+let myСity = "Kyiv";
+
+console.log(myName, mySurmame, myСity);
+
+console.log(typeof myName); // demonstrate the variable type in the console;
+
+const oneWeek = 7;
+
+const twoWeeks = 14;
+
+let oneMonth = 30;
+
+const oneYear = 365;
+
+let arithmeticMean = (oneWeek + twoWeeks + oneMonth + oneYear) / 4;
+
+console.log(arithmeticMean);
+
+let circleRadius = 10;
+
+console.log(circleRadius);
+
+/* 
+  формула для обчислення площі кола:
+  S = Math.PI * circleRadius * circleRadius;
+*/
+
+let circleArea = Math.PI * circleRadius * circleRadius;
+
+console.log(circleArea);
+
+/* 
+  формула для обчислення довжини кола:
+  С = 2 * Math.PI * circleRadius;
+*/
+
+let circumference = 2 * Math.PI * circleRadius;
+
+console.log(circumference);
+
+// Неправильні змінні:
+
+// let var = "Boris";
+
+const oneHour = "60 minutes";
+
+// const oneHour = "50 minutes";
+
+// let 2_class = 10; 
