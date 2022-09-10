ba63a45a8dcd847176cf56decd34b39563415adf
Lunnisha <107049777+Lunnisha@users.noreply.github.com>
2022-09-11 00:31:11 +0300

    add files

---
+++ scripts/scripts-1.js
@@ -0,0 +1,58 @@
+const userName = prompt("Яке Ваше імʼя?");
+
+const userSurname = prompt("Яке Ваше прізвище?");
+
+const userFavoriteNumber = +prompt("Яке Ваше улюблене число?");
+
+alert(`Вітаю, ${userName} ${userSurname}! Ваше улюблене число - ${userFavoriteNumber}`);
+
+console.log(`${userName} ${userSurname} ${userFavoriteNumber}`);
+
+const firstClientNumber = +prompt("Введіть перше число");
+
+const secondClientNumber = +prompt("Введіть друге число");
+
+const addition = (firstClientNumber + secondClientNumber);
+
+const subtraction = (firstClientNumber - secondClientNumber);
+
+const multiplication = (firstClientNumber * secondClientNumber);
+
+const division = (firstClientNumber / secondClientNumber);
+
+alert(`Сума - ${addition}, різниця - ${subtraction}, добуток - ${multiplication}, ділення - ${division}`);
+
+console.log(`Сума - ${addition}, різниця - ${subtraction}, добуток - ${multiplication}, ділення - ${division}`);
+
+const userAgeQuestion = +prompt("В якому році Ви народились");
+
+let howOldIsTheUser;
+
+const dataNow = 2022;
+
+const userAgeResult = (2022 - userAgeQuestion);
+
+if (userAgeResult >= 18) howOldIsTheUser = "Вітаю, Ви - дорослий, можете пити пиво :)";
+
+else if (userAgeResult >= 16) howOldIsTheUser = "Вітаю, Ви - підліток, можете пити лимонад :)";
+
+else if (userAgeResult <= 15) howOldIsTheUser = "Вітаю, Ви - дитина, можете пити те, що дозволять батьки :)";
+
+alert(`Ваш вік - ${userAgeResult} років. ${howOldIsTheUser}`);
+
+console.log(`Ваш вік - ${userAgeResult} років. ${howOldIsTheUser}`);
+
+// Остача від ділення
+
+const firstNumberOfDivisions = 20;
+
+const secondNumberOfDivisions = 3;
+
+const resultOfDivisions = (firstNumberOfDivisions % secondNumberOfDivisions);
+
+alert(`Остача від ділення числа ${firstNumberOfDivisions} на число ${secondNumberOfDivisions} рівна ${resultOfDivisions}`);
+
+console.log(`Остача від ділення числа ${firstNumberOfDivisions} на число ${secondNumberOfDivisions} рівна ${resultOfDivisions}.`);
+
+// Парне чи не парне
+
